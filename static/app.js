9b907e3a9cfc3a06f5e1d9b883ba6a9934620bdd
abe <el.lauwer@gmail.com>
2011-10-01 02:49:50 +0200

    Fixed commets and removed buttons

--- static/app.js
+++ static/app.js
@@ -12,19 +12,19 @@ window.App = (function($) {
         $.ajax(url);
     };
 
-    $('#light-one-on').bind('click', function() {lightHandler(3, true);});
-    $('#light-one-off').bind('click', function() {lightHandler(3, false);});
+    //$('#light-one-on').bind('click', function() {lightHandler(3, true);});
+    //$('#light-one-off').bind('click', function() {lightHandler(3, false);});
     $('#light-two-on').bind('click', function() {lightHandler(4, true);});
     $('#light-two-off').bind('click', function() {lightHandler(4, false);});
     $('#light-three-on').bind('click', function() {lightHandler(5, true);});
     $('#light-three-off').bind('click', function() {lightHandler(5, false);});
 
-    $('#light-all-off').bind('click', function() {
-        $.ajax('http://' + HOSTNAME + '/api/switch_all_off');
-    });
-    $('#light-all-on').bind('click', function() {
-        $.ajax('http://' + HOSTNAME + '/api/switch_all_on');
-    });
+    //$('#light-all-off').bind('click', function() {
+        //$.ajax('http://' + HOSTNAME + '/api/switch_all_off');
+    //});
+    //$('#light-all-on').bind('click', function() {
+        //$.ajax('http://' + HOSTNAME + '/api/switch_all_on');
+    //});
 
     // Coffee
     var coffeeHandler = function(id) {
@@ -32,7 +32,7 @@ window.App = (function($) {
         $.ajax(url);
     };
     $('#coffee-power').bind('click', function() {coffeeHandler(0);});
-    $('#coffee-small').bind('click', function() {coffeeHandler(1);});
+    $('#coffee-small').bind('click', function() {coffeeHandler(2);});
     $('#coffee-large').bind('click', function() {coffeeHandler(2);});
 });
 
